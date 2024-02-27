9002a2df9da2a692dc18025f84b865a4a6dd78f3
DevWithEasy <devwitheasy@gmail.com>
2024-02-27 18:58:06 +0600

    authentication add in rooute

--- routers/businessRouters.js
+++ routers/businessRouters.js
@@ -1,18 +1,19 @@
 const { createBusiness, getBusiness, updateBusiness, deleteBusiness, getInfo, memberConfirm, memberRoleChange, memberRemove, memberOwnerChange, memberVerify } = require('../controllers/businessControllers')
+const authenticated = require('../middleware/authenticated')
 
 const router = require('express').Router()
 
-router.post('/',createBusiness)
-.get('/:id',)
-.get('/',getBusiness)
-.put('/:id',updateBusiness)
-.delete('/:id',deleteBusiness)
-.get('/info',getInfo)
-.put('/member-confirm',memberConfirm)
-.put('/member-owner-change',memberOwnerChange)
-.put('/member-remove',memberRemove)
-.put('/member-role-change',memberRoleChange)
-.put('/member-verify',memberVerify)
+router.post('/',authenticated,createBusiness)
+.get('/:id',authenticated,)
+.get('/',authenticated,getBusiness)
+.put('/:id',authenticated,updateBusiness)
+.delete('/:id',authenticated,deleteBusiness)
+.get('/info',authenticated,getInfo)
+.put('/member-confirm',authenticated,memberConfirm)
+.put('/member-owner-change',authenticated,memberOwnerChange)
+.put('/member-remove',authenticated,memberRemove)
+.put('/member-role-change',authenticated,memberRoleChange)
+.put('/member-verify',authenticated,memberVerify)
 
 
 module.exports = router
--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,13 +1,14 @@
 const { accountConfirm, checking, invitionAccept, sendOTP, uploadImage, verifyOTP } =require('../controllers/userControllers'
 ) 
+const authenticated = require('../middleware/authenticated')
 const router = require('express').Router()
 
 router.post('/send-otp',sendOTP)
 .post('/verify-otp',verifyOTP)
 .post('/account-confirm',accountConfirm)
 .post('/invitition-accept',invitionAccept)
-.get('/checking',checking)
-.put('/upload',uploadImage)
+.get('/checking',authenticated,checking)
+.put('/upload',authenticated,uploadImage)
 
 
 module.exports = router
