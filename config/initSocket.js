22dd73afd72d936a1191da50724893c04404195e
DevWithEasy <devwitheasy@gmail.com>
2024-03-04 23:39:48 +0600

    socket code update

--- config/initSocket.js
+++ config/initSocket.js
@@ -8,33 +8,67 @@ const initSocket = (server) => {
     })
 
     io.on('connection', (socket) => {
+
         socket.on('join_cashbook', data => {
             socket.join(data._id)
         })
-        socket.on('update_business',data=>{
-            const {business} = data
-            if(business?.teams.length > 0){
-                business?.teams.forEach(member=>{
-                    socket.to(member.user._id).emit('update_business_client',business)
+        //=============Business==============
+        socket.on('update_business', data => {
+            const { business } = data
+            if (business?.teams.length > 0) {
+                business?.teams.forEach(member => {
+                    socket.to(member.user._id).emit('update_business_client', business)
                 })
             }
         })
 
-        socket.on('add_business',data=>{
-            const {_id,business} = data
-            socket.to(_id).emit('add_business_client',business)
+        socket.on('add_business', data => {
+            const { _id, business } = data
+            socket.to(_id).emit('add_business_client', business)
         })
 
-        socket.on('remove_business',data=>{
-            const {_id,b_id,business} = data
-            socket.to(_id).emit('remove_business_client',{id : b_id})
-            
-            if(business?.teams.length > 0){
-                business?.teams.forEach(member=>{
-                    socket.to(member.user._id).emit('update_business_client',business)
+        socket.on('remove_business', data => {
+            const { _id, b_id, business } = data
+            socket.to(_id).emit('remove_business_client', { id: b_id })
+
+            if (business?.teams.length > 0) {
+                business?.teams.forEach(member => {
+                    socket.to(member.user._id).emit('update_business_client', business)
                 })
             }
         })
+        //=============Business==============
+
+        //=============Book==================
+        socket.on('update_book',data=>{
+            const {book} = data
+            if (book?.members.length > 0) {
+                book?.members.forEach(member => {
+                    socket.to(member.user._id).emit('update_book_client', book)
+                })
+            }
+        })
+        //book member
+        socket.on('add_team', data => {
+            const { _id, book } = data
+            socket.to(_id).emit('add_team_client', book)
+            if (book?.members.length > 0) {
+                book?.members.forEach(member => {
+                    socket.to(member.user._id).emit('update_book_client', book)
+                })
+            }
+        })
+
+        socket.on('remove_team', data => {
+            const { _id, book } = data
+            socket.to(_id).emit('remove_team_client', book)
+            if (book?.members.length > 0) {
+                book?.members.forEach(member => {
+                    socket.to(member.user._id).emit('update_book_client', book)
+                })
+            }
+        })
+        //=============Book=================
     })
 }
 
